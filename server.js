e5fd87e4f4918add65246f69be184ec36e31d4e4
Brendan McMahon <brendanjeffreymcmahon@gmail.com>
2020-09-09 15:37:09 -0400

    use compression

--- server.js
+++ server.js
@@ -2,8 +2,10 @@
 const express = require('express');
 const path = require('path');
 
+const compression = require('compression');
 const app = express();
 
+app.use(compression());
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/marital-bliss-ui'));
 

7d60bc0f34980a2c608d6f54305fd4b9b6f98661
Brendan McMahon <brendanjeffreymcmahon@gmail.com>
2020-09-10 10:37:26 -0400

    http => https redirect

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+const sslRedirect = require('heroku-ssl-redirect');
 
 const compression = require('compression');
 const app = express();
 
 app.use(compression());
+app.use(sslRedirect());
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/marital-bliss-ui'));
 
